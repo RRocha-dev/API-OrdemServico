aa57e6e9a55726a28324c72d1ee51305c43493e8
Rickelme Rocha <rickelme.rocha.andrade@gmail.com>
2024-07-28 10:23:06 -0300

    Commit Inicial

---
+++ src/main/java/br/com/rickrocha/apiordemservico/ApiOrdemServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rickrocha.apiordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrdemServicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiOrdemServicoApplication.class, args);
+    }
+
+}
